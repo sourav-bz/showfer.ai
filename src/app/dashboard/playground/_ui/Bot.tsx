b7c69b400734332afa055a13856bb5f5014ecfd6
sourav-bz <s.kumar001997@gmail.com>
2024-08-29 12:28:25 +0530

    orb animation using css added

--- src/app/dashboard/playground/_ui/Bot.tsx
+++ src/app/dashboard/playground/_ui/Bot.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
-import Image from "next/image";
+import Lottie from "lottie-react";
 import BotWindow from "./BotWindow";
+import orbAnimation from "../../../../../public/playground/lottie/orb.json";
+import voiceAnimation from "../../../../../public/playground/lottie/voiceanimation.json";
+import voiceOrb from "../../../../../public/playground/lottie/voiceOrb.json";
+import AnimatedOrb from "./AnimatedOrb";
 
 const Bot = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,12 +15,7 @@ const Bot = () => {
     <div className="absolute bottom-[70px] right-4 flex flex-col items-end">
       {isOpen && <BotWindow />}
       <button onClick={toggleChat}>
-        <Image
-          src={"./playground/orb.svg"}
-          width={50}
-          height={50}
-          alt="chat-icon"
-        />
+        <AnimatedOrb width={50} height={50} />
       </button>
     </div>
   );
--- src/app/dashboard/playground/_ui/VoiceInterface.tsx
+++ src/app/dashboard/playground/_ui/VoiceInterface.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
+import VoiceOrb from "./VoiceOrb";
 
 export default function VoiceInterface() {
   return (
     <div className="h-full p-6 flex flex-col justify-center items-center">
-      <Image
-        src={"./playground/circular-orb.svg"}
-        alt="orb"
-        width={300}
-        height={300}
-      />
+      <div className="mb-8">
+        <VoiceOrb width={200} height={200} />
+      </div>
       <Image src={"./playground/mic.svg"} width={70} height={70} alt="mic" />
     </div>
   );
