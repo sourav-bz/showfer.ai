a65da315831058505a1536e344d5202739e1bae2
sourav-bz <s.kumar001997@gmail.com>
2024-09-24 00:54:32 +0530

    fixed document on mounted

--- src/app/(landing)/_ui/Navbar.tsx
+++ src/app/(landing)/_ui/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -8,6 +8,12 @@ import { PopupButton } from "react-calendly";
 
 const Navbar: React.FC = () => {
   const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <nav className="bg-white p-3 shadow-sm mb-4 rounded-lg">
       <div className=" flex justify-between items-center">
@@ -22,11 +28,13 @@ const Navbar: React.FC = () => {
         <div className="justify-end items-center gap-[15px] flex">
           <div className="px-5 py-2.5 bg-[#f0f2f7] rounded-md justify-center items-center gap-2.5 flex cursor-pointer">
             <div className="text-[#6d67e4] text-sm font-medium tracking-tight">
-              <PopupButton
-                url="https://calendly.com/showfer-support/demo"
-                text="Schedule a demo"
-                rootElement={document.body}
-              />
+              {isMounted && (
+                <PopupButton
+                  url="https://calendly.com/showfer-support/demo"
+                  text="Schedule a demo"
+                  rootElement={document.body}
+                />
+              )}
             </div>
           </div>
           <a
